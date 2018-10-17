65f071bf303964e1cdbc12b6c365955ca4690519
Marco Fernandez Pranno <marco.fp94@gmail.com>
2018-10-17 23:30:33 +0200

    Using a map instead of an object

--- src/classifyDuplicates/printDuplicates.js
+++ src/classifyDuplicates/printDuplicates.js
@@ -3,14 +3,14 @@ const printDuplicates = (values, log) => {
     throw new Error('printDuplicates - Wrong arguments');
   }
 
-  const uniqueValues = {};
+  const uniqueValues = new Map();
 
   for (let i = 0; i < values.length; i += 1) {
     const value = values[i];
-    if (!uniqueValues[value]) {
-      uniqueValues[value] = 1;
-    } else if (uniqueValues[value] === 1) {
-      uniqueValues[value] = 2;
+    if (!uniqueValues.get(value)) {
+      uniqueValues.set(value, 1);
+    } else if (uniqueValues.get(value) === 1) {
+      uniqueValues.set(value, 2);
       log(value);
     }
   }
